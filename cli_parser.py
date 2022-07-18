3bbbfb3f039ba9a8106d9cfa49b145c519df3e9e
Spooghetti420 <kkovachev1947@gmail.com>
2022-07-18 20:23:21 +0100

    Move test program to main.py

--- cli_parser.py
+++ cli_parser.py
@@ -126,7 +126,7 @@ class CLIParser(ABC):
     def _post_parsing_check(self):
         for argument in self.arguments.values():
             if not argument.satisfied:
-                logging.warning(f"WARNING: Argument did not receive enough arguments: {argument.name}")
+                logging.warning(f"argument did not receive enough arguments: {argument.name}")
 
     @classmethod
     def add_argument(cls, name, arg: CLIArgument) -> None:
@@ -231,24 +231,3 @@ def parser(cls: CLIParser):
         return _create_args(cls)
     
     return wrap(cls)
-
-
-if __name__ == "__main__":
-    @parser
-    class TestCLIParser(CLIParser):
-        help: CLIFlag(help_str="Prints help message as to how to use the program.")
-        n: CLIArgument( arg_type=int, nargs=1, switch=True)
-        data: CLIArgument(arg_type=int, nargs=4)
-        l: CLIFlag(help_str="Flag l.")
-        i: CLIFlag(help_str="Flag i.")
-        s: CLIFlag(help_str="Flag s.")
-        a: CLIFlag(help_str="Flag a.")
-
-
-def main():
-    parser = TestCLIParser()
-    parser.parse(sys.argv[1:])
-    print(parser)
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import sys
 from cli_parser import CLIArgument, CLIFlag, parser, CLIParser
 
 
-@parser
-class LanguageCLIParser(CLIParser):
-    help: CLIFlag()
+if __name__ == "__main__":
+    @parser
+    class TestCLIParser(CLIParser):
+        help: CLIFlag(help_str="Prints help message as to how to use the program.")
+        n: CLIArgument( arg_type=int, nargs=1, switch=True)
+        data: CLIArgument(arg_type=int, nargs=4)
+        l: CLIFlag(help_str="Flag l.")
+        i: CLIFlag(help_str="Flag i.")
+        s: CLIFlag(help_str="Flag s.")
+        a: CLIFlag(help_str="Flag a.")
 
 
 def main():
-    parser = LanguageCLIParser()
+    parser = TestCLIParser()
     parser.parse(sys.argv[1:])
-    if parser["help"]:
-        print("usage: main.py <INPUT FILE>")
-    else:
-        print("main.py running normally")
-    
+
 if __name__ == "__main__":
     main()
